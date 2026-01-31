b9d27dd8c3422759d20691b16414fdb3bf6cad9a
Nikhil <nikatfinest@gmail.com>
2026-01-31 08:16:32 +0545

    Signup commit

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -4,12 +4,38 @@ export default function Signup() {
   return (
     <div className="container">
       <div className="row justify-content-center">
-        <div className="col-md-6 col-lg-4">
+        <div className="col-md-6 col-lg-5">
           <div className="card shadow-sm mt-5">
             <div className="card-body p-4">
               <h2 className="card-title text-center mb-4">Sign Up</h2>
               
               <form>
+                <div className="row">
+                  <div className="col-md-6 mb-3">
+                    <label htmlFor="firstNameInput" className="form-label">
+                      First Name
+                    </label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      id="firstNameInput"
+                      placeholder="John"
+                    />
+                  </div>
+
+                  <div className="col-md-6 mb-3">
+                    <label htmlFor="lastNameInput" className="form-label">
+                      Last Name
+                    </label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      id="lastNameInput"
+                      placeholder="Doe"
+                    />
+                  </div>
+                </div>
+
                 <div className="mb-3">
                   <label htmlFor="emailInput" className="form-label">
                     Email address
